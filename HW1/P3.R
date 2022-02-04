3cf20a7c22ac7c6e05e1eb769a57a7a3e927ec30
Mukai Wang <wangmk@umich.edu>
2022-02-03 22:30:05 -0500

    Finish question 1,2,3, and 5.

--- HW1/P3.R
+++ HW1/P3.R
@@ -4,6 +4,7 @@ Rcpp::sourceCpp('HW1/P3.cpp')
 
 simulation = as.vector(mc3(X0=0))
 
+plot(simulation, type='l', xlab="Iteration", ylab="Value", main="Random Walk Markov Chain")
 
 est_distribution = as.vector(mstep3(simulation, 4, 3))
 
