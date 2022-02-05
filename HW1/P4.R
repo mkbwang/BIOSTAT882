9d50eb1fb56dbf5d46e22910e986466738b8af30
Mukai Wang <wangmk@umich.edu>
2022-02-04 21:53:29 -0500

    Finish HW1.

--- HW1/P4.R
+++ HW1/P4.R
@@ -15,4 +15,11 @@ for (j in 1:50){
   est_params2[j] = fitdistr(mat2[j, 9001:10000], 'exponential')$estimate
 }
 
+comparison <- data.frame(Estimate = c(est_params1, est_params2),
+                         Proposal = rep(c(0.1, 5), each=50))
+comparison$Proposal <- as.factor(comparison$Proposal)
 
+library(ggplot2)
+ggplot(comparison, aes(x=Proposal, y=Estimate)) + geom_boxplot() + 
+  xlab("Rate of Proposal Exponential Distribution") + ylab("Values") + 
+  ggtitle("Posterior Estimate of Exponential Distribution Rate")
