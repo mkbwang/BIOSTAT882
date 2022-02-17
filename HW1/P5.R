ca747595aa41987b70abab204d702882e15b74b0
Mukai Wang <wangmk@umich.edu>
2022-02-17 08:25:58 -0500

    Update the PDF. The results seem fine now.

--- HW1/P5.R
+++ HW1/P5.R
@@ -6,24 +6,24 @@ yvec = rnorm(100, 10, 1)
 sourceCpp("HW1/P5.cpp")
 
 
-result <- langevin5(yvec, chain_length=30000, sigma02=1e-8, 
-                      init_mu1 = 5, init_mu2 = 5)
+result <- langevin5(yvec, chain_length=40000, sigma02=1e-8, 
+                      init_mu1 = 5, init_mu2 = 5, rw=FALSE)
 
 # loglik
-plot(x=25000:30000, result$loglik[25000:30000], type='l')
+plot(x=1:40000, result$loglik[1:40000], type='l', xlab="Step", ylab="Log Posterior Likelihood")
 
 # acceptance
-plot(result$acceptance, type='l',
+plot(x=seq(1, 40000, 100), y=result$acceptance, type='l',
      xlab = "Step", ylab="Average Rate", main="Average Acceptance Rate every 100 steps")
 
 #mu_1
-plot(x=25000:30000, y=result$mu1[25000:30000], type='l',xlab="Iteration", ylab="Estimated Value", main=expression(mu[1]))
-hist(result$mu1[25000:30000], xlab="Value", ylab="Count", main=expression(mu[1]))
-mean(result$mu1[25000:30000])
-sd(result$mu1[25000:30000])
+plot(x=35000:40000, y=result$mu1[35000:40000], type='l',xlab="Iteration", ylab="Estimated Value", main=expression(mu[1]))
+hist(result$mu1[35000:40000], xlab="Value", ylab="Count", main=expression(mu[1]))
+mean(result$mu1[35000:40000])
+sd(result$mu1[35000:40000])
 
 #mu_2
-plot(x=25000:30000, y=result$mu2[25000:30000], type='l',xlab="Iteration", ylab="Estimated Value", main=expression(mu[2]))
+plot(x=35000:40000, y=result$mu2[35000:40000], type='l',xlab="Iteration", ylab="Estimated Value", main=expression(mu[2]))
 hist(result$mu2[25000:30000], xlab="Value", ylab="Count", main=expression(mu[2]))
 mean(result$mu2[25000:30000])
 sd(result$mu2[25000:30000])
--- HW1/P5.cpp
+++ HW1/P5.cpp
@@ -8,7 +8,7 @@ using namespace Rcpp;
 
 
 // calculate the threshold for acceptance/rejection
-NumericVector logthreshold(NumericVector Y, double mu1, double mu2, double mu1_prime, double mu2_prime, double sigma2){
+NumericVector logthreshold(NumericVector Y, double mu1, double mu2, double mu1_prime, double mu2_prime, double sigma2, bool rw=false){
   NumericVector deviation = Y - mu1 - mu2;
   NumericVector deviation2 = deviation * deviation;
   double deviation_sum = sum(deviation);
@@ -24,18 +24,30 @@ NumericVector logthreshold(NumericVector Y, double mu1, double mu2, double mu1_p
     1/2*deviation_prime2_sum;
   
   // proposal distribution of mu given mu_prime
-  double diff_mu1_mu1prime = mu1 - mu1_prime - sigma2 / 2 * 
-    (-mu1_prime + deviation_prime_sum);
-  double diff_mu2_mu2prime = mu2 - mu2_prime - sigma2 / 2 * 
-    (-1000*mu2_prime + deviation_prime_sum);
+  double diff_mu1_mu1prime, diff_mu2_mu2prime;
+  if (rw){
+    diff_mu1_mu1prime = mu1 - mu1_prime ;
+    diff_mu2_mu2prime = mu2 - mu2_prime ;
+  } else{
+    diff_mu1_mu1prime = mu1 - mu1_prime - sigma2 / 2 * 
+      (-mu1_prime + deviation_prime_sum);
+    diff_mu2_mu2prime = mu2 - mu2_prime - sigma2 / 2 * 
+      (-1000*mu2_prime + deviation_prime_sum);
+  }
   double log_proposal_mu_from_muprime = -1/(2*sigma2)*
     (diff_mu1_mu1prime * diff_mu1_mu1prime + diff_mu2_mu2prime * diff_mu2_mu2prime);
   
   // proposal distribution of mu_prime given mu
-  double diff_mu1prime_mu1 = mu1_prime - mu1 - sigma2 / 2 * 
-    (-mu1 + deviation_sum);
-  double diff_mu2prime_mu2 = mu2_prime - mu2 - sigma2 / 2 * 
-    (-1000*mu2 + deviation_sum);
+  double diff_mu1prime_mu1, diff_mu2prime_mu2;
+  if (rw){
+    diff_mu1prime_mu1 = mu1_prime - mu1 - sigma2 / 2 * 
+      (-mu1 + deviation_sum);
+    diff_mu2prime_mu2 = mu2_prime - mu2 - sigma2 / 2 * 
+      (-1000*mu2 + deviation_sum);
+  } else{
+    diff_mu1prime_mu1 = mu1_prime - mu1;
+    diff_mu2prime_mu2 = mu2_prime - mu2;
+  }
   double log_proposal_muprime_from_mu = -1/(2*sigma2)*
     (diff_mu1prime_mu1 * diff_mu1prime_mu1 + diff_mu2prime_mu2 * diff_mu2prime_mu2);
   
@@ -48,7 +60,7 @@ NumericVector logthreshold(NumericVector Y, double mu1, double mu2, double mu1_p
 
 
 // [[Rcpp::export]]
-List langevin5(NumericVector Y, int chain_length=10000, double sigma02=0.001, int period=100, double init_mu1=5, double init_mu2=5) {
+List langevin5(NumericVector Y, int chain_length=10000, double sigma02=0.001, int period=100, double init_mu1=5, double init_mu2=5, bool rw=false) {
   NumericVector mu1(chain_length+1);
   NumericVector mu2(chain_length+1);
   NumericVector loglik(chain_length);
@@ -62,12 +74,19 @@ List langevin5(NumericVector Y, int chain_length=10000, double sigma02=0.001, in
     // diffusion
     NumericVector Z=Rcpp::rnorm(2, 0, 1);
     double deviation_sum = sum(Y - mu1[i] - mu2[i]);
-    double mu1_prime = mu1[i] + sigma2 / 2 * 
-      (-mu1[i] + deviation_sum) +
-      Z[0] * std::sqrt(sigma2);
-    double mu2_prime = mu2[i] + sigma2 / 2 * 
-      (-1000*mu2[i] + deviation_sum)+
-      Z[1] * std::sqrt(sigma2);
+    double mu1_prime, mu2_prime;
+    if (rw){
+      mu1_prime = mu1[i] + Z[0] * std::sqrt(sigma2);
+      mu2_prime = mu2[i] + Z[1] * std::sqrt(sigma2);
+    } else{
+      mu1_prime = mu1[i] + sigma2 / 2 * 
+        (-mu1[i] + deviation_sum) +
+        Z[0] * std::sqrt(sigma2);
+      mu2_prime = mu2[i] + sigma2 / 2 * 
+        (-1000*mu2[i] + deviation_sum)+
+        Z[1] * std::sqrt(sigma2);
+    }
+    
     
     // accept or reject
     double U = R::runif(0, 1);
